d9d3334cdfb48617febb6beb3c2135bec4a51084
bartoszsab123 <bartoszsab123@wp.pl>
2026-01-20 17:09:18 +0100

    ko

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public boolean isEven(int x) {
+        return x % 2 == 0;
+    }
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,38 @@
+import org.example.Calculator;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void test1(){
+        int a = 4;
+        int b = 6;
+        assert a + b == 10;
+    }
+
+    @Test
+    void test2(){
+        int x = 9;
+        assert x % 2 != 0;
+    }
+
+    @Test
+    void test3(){
+        int x = 8;
+        assert x / 2 == 4;
+    }
+
+    @Test
+    void test4(){
+        Calculator cal = new Calculator();
+        int result = cal.add(3, 7);
+        assert result == 10;
+    }
+
+    @Test
+    void test5(){
+        Calculator cal = new Calculator();
+        boolean result = cal.isEven(6);
+        assert result == true;
+    }
+}
